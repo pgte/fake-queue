54f6f748c74d336ae713a641551ec594990c4b3c
Pedro Teixeira <pedro.teixeira@gmail.com>
2011-09-10 23:09:29 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# fake-queue
+
+In-memory queue for Node.js.
+
+Used initially for simulating a queueing system for testing banzai.
+
+# Installation
+
+    $ cd <PROJECT ROOT>
+    $ npm install fake-queue
+
+# Usage
+
+## Job producer:
+
+    var queue  = require('fake-queue')
+
+    var job = {
+        to: 'pedro.teixeira@gmail.com'
+      , subject: 'Oh hay!'
+      , body: 'Hello world!'
+    };
+
+    queue.push('email', job);
+
+
+## Job consumer:
+
+    var queue = require('fake-queue);
+
+    queue.pop('email', function(job, done) {
+      // actally send the email (not done here);
+
+      // call done, with no error, or done(err) if there is an error
+      done();
+    });
---
+++ index.js
@@ -0,0 +1,40 @@
+var queues = {};
+var waiting = {};
+
+function notify(type) {
+  var clients = waiting[type]
+    , queue = queues[type]
+    , client
+    , job;
+    
+  if (queue && clients) {
+    while (queue.length > 0 && clients.length > 0) {
+      client = clients.splice(0, 1)[0];
+      job = queue.splice(0, 1)[0];
+      (function(client, job) {
+        process.nextTick(function() {
+          client(job, function(err) {
+            if (err) { push(type, job); }
+          });
+        });
+      })(client, job);
+    }
+  }
+}
+
+function push(type, jobData) {
+  if (! queues[type]) { queues[type] = []; }
+  queues[type].push(jobData);
+  notify(type);
+}
+
+function pop(type, callback) {
+  if (! waiting[type]) { waiting[type] = []; }
+  waiting[type].push(callback);
+  notify(type);
+}
+
+module.exports = {
+    push: push
+  , pop: pop
+};
